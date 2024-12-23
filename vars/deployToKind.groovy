364449d81166ec56de90e17b8d68582dfb12ee5f
Muhammad Gamal <59343923+mujemi26@users.noreply.github.com>
2024-12-23 19:07:40 +0200

    Update deployToKind.groovy

--- vars/deployToKind.groovy
+++ vars/deployToKind.groovy
@@ -1,34 +1,34 @@
-    def call(Map config = [:]) {
-      def imageName = config.imageName
-      def version = config.version
-      def deploymentName = config.deploymentName
-      def containerName = config.containerName
-
-      withCredentials([string(credentialsId: 'kind-kubeconfig', variable: 'KUBECONFIG')]) {
-        sh """
-         echo "\${KUBECONFIG}" > kubeconfig.tmp
-         kubectl apply --kubeconfig kubeconfig.tmp -f - <<EOF
+ def call(Map config = [:]) {
+    def imageName = config.imageName
+     def version = config.version
+    def deploymentName = config.deploymentName
+    def containerName = config.containerName
+   
+    withCredentials([string(credentialsId: 'kind-kubeconfig', variable: 'KUBECONFIG')]) {
+       sh """
+          echo "\${KUBECONFIG}".replaceAll('"', '\\\\"') > kubeconfig.tmp
+             kubectl apply --kubeconfig kubeconfig.tmp -f - <<EOF
               apiVersion: apps/v1
              kind: Deployment
-             metadata:
-               name: ${deploymentName}
+              metadata:
+                name: ${deploymentName}
              spec:
-                selector:
+               selector:
                   matchLabels:
-                    app: ${containerName}
-                 replicas: 1
-             template:
-                metadata:
+                     app: ${containerName}
+                replicas: 1
+              template:
+                 metadata:
                    labels:
-                    app: ${containerName}
-                spec:
-                  containers:
+                     app: ${containerName}
+               spec:
+                 containers:
                     - name: ${containerName}
                       image: "${imageName}:${version}"
-                     ports:
-                        - containerPort: 8080
+                    ports:
+                      - containerPort: 8080
            EOF
           rm kubeconfig.tmp
-           """
-       }
-     }
+         """
+        }
+  }
