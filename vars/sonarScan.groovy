06364e8902e26bea54547eb072d1344d46ab1cb7
Muhammad Gamal <59343923+mujemi26@users.noreply.github.com>
2024-12-23 15:51:39 +0200

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,14 +1,17 @@
-    def call(Map config = [:]) {
-       def projectKey = config.projectKey
-       def projectName = config.projectName
-       def sonarHostUrl = config.sonarHostUrl
-
-        withSonarQubeEnv('mySonarQube') {
-          sh """
+def call(Map config = [:]) {
+      def projectKey = config.projectKey
+      def projectName = config.projectName
+      def sonarHostUrl = config.sonarHostUrl
+    
+      withSonarQubeEnv('mySonarQube') {
+        withCredentials([string(credentialsId: 'sonarqube-token', variable: 'SONAR_TOKEN')]) {
+            sh """
             ./gradlew sonar \
                 -Dsonar.projectKey=${projectKey} \
-                 -Dsonar.projectName="${projectName}" \
-                 -Dsonar.host.url=${sonarHostUrl}
-           """
+                -Dsonar.projectName="${projectName}" \
+                 -Dsonar.host.url=${sonarHostUrl} \
+                 -Dsonar.login=$SONAR_TOKEN
+             """
+          }
         }
-     }
+   }
