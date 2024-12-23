d9396ae6001c65284d4d8234d309179fa38309bb
Muhammad Gamal <59343923+mujemi26@users.noreply.github.com>
2024-12-23 17:28:46 +0200

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -6,7 +6,7 @@
          withSonarQubeEnv('mySonarQube') {
              withCredentials([string(credentialsId: 'sonarqube-token', variable: 'SONAR_TOKEN')]) {
                 sh """
-                     ./gradlew sonar
+                     ./gradlew sonar --stacktrace
                  """
                 sh """
                    ./gradlew -Dsonar.login=\${SONAR_TOKEN} \
